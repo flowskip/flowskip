c3e48796eef7d18afa2f54db1ec13c86d700c0dd
SantaigoGomez12 <sgomezsalazar7@gmail.com>
2021-08-06 21:27:46 -0500

    agregando el loader

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import ConfigRoom from "../pages/ConfigRoom";
 import RedirectFromApi from "./RedirectFromApi.js";
 import Room from "../pages/Room";
+import Loader from "../components/loader";
 
 import GlobalStyle from "../styles/GlobalStyle";
 
@@ -17,6 +18,7 @@ export default function App() {
         <Route exact path="/config-room" component={ConfigRoom} />
         <Route exact path="/redirect-from-api" component={RedirectFromApi} />
         <Route exact path="/room/:room_code" component={Room} />
+        <Route exact path="/loader.js" component={Loader} />
       </Switch>
     </BrowserRouter>
   );
